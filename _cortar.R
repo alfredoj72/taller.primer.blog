6074c31cad3ba9df4463c7bab90e93b23b650d2c
Pedro J. Pérez <pedro.j.perez@uv.es>
2022-10-24 11:51:14 +0200

    puliendo cera

--- _cortar.R
+++ _cortar.R
@@ -1,13 +1,28 @@
 ```{r, eval = FALSE}
 #| code-overflow: wrap   #- {scroll, wrap}
 
-# 1/ Opciones referentes al proyecto ----------------------------
-project:
-  type: website
-output-dir: docs #-- la carpeta donde se redirige el output (.html)
-# execute-dir: project #-- the working directory for rendering files within a project is the directory of the file itself. If you prefer to use the main project directory instead, you can add the execute-dir: project https://quarto.org/docs/projects/code-execution.html#working-dir
 
 
 
 #| code-line-numbers: true
 #| code-overflow: wrap
+
+
+
+
+:::: {.columns}
+
+::: {.column width="48%"}
+
+:::
+  
+::: {.column width="4%"}
+<!-- empty column to create gap -->
+:::
+  
+::: {.column width="48%"}
+
+
+:::
+  
+::::
---
+++ _hacer.R
@@ -0,0 +1,25 @@
+#- lista para hacer ------------------------------------------------------------
+
+#- 6 productivity hacks: https://www.rstudio.com/blog/6-productivity-hacks-for-quarto/#shortcodes
+
+#- includes: https://mine-cetinkaya-rundel.github.io/quarto-tip-a-day/posts/04-include/
+
+#- 1) extensiones, y en concreto insertar videos: https://github.com/sellorm/quarto-social-embeds
+#- 2) insertar una web (lo tengo en slides presentar)
+
+#- Iluminar algunas lineas de código (tt_05)
+#- Poner más opciones de yaml (tt_05)
+
+#- SLIDES ----------------------------------------------------------------------
+#- poner en una esquinar el nº de tutorial x el q vamos
+#- areglar títulos de las slides (color, centrado , tamaño y más espacio abajo)
+
+
+## Inline
+
+
+```markdown
+## Circle
+
+The radius of the circle is `r radius`.
+```
